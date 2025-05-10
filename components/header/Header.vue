34f0a40727e3ddddddb08c29bba484cc3b3bacc6
Georgii <maratakoev@gmail.com>
2025-05-10 20:01:47 +0300

    оптимизировано меню

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -22,47 +22,7 @@
         </router-link>
         <burger></burger>
       </nav>
-      <div class="header__submenu" :style="submenuStyle" @mouseenter="cancelHide" @mouseleave="hideSubMenu">
-        <div class="header__submenu-column">
-          <span class="header__submenu-title">Наши экскурсии</span>
-          <ul class="header__submenu-list">
-            <li 
-              v-for="(item, index) in submenuItems" 
-              :key="index" 
-              class="header__submenu-subtitle" 
-              :style="getItemStyle(index)"
-            >
-            <router-link :to="item.route" @click="hideSubMenu">{{ item.label }}</router-link>
-          </li>
-          </ul>
-        </div>
-        <div class="header__submenu-column">
-          <span class="header__submenu-title">Дополнительно</span>
-          <ul class="header__submenu-list">
-            <li 
-              v-for="(item, index) in additionalItems" 
-              :key="index" 
-              class="header__submenu-item" 
-              :style="getItemStyle(index)"
-            >
-              <router-link :to="item.route" @click="hideSubMenu">{{ item.label }}</router-link>
-            </li>
-          </ul>
-        </div>
-        <div class="header__submenu-column">
-          <span class="header__submenu-title">Подробности</span>
-          <ul class="header__submenu-list">
-            <li 
-              v-for="(item, index) in discriptionItems" 
-              :key="index" 
-              class="header__submenu-item" 
-              :style="getItemStyle(index)"
-            >
-            <router-link :to="item.route" @click="hideSubMenu">{{ item.label }}</router-link>
-          </li>
-          </ul>
-        </div>
-      </div>
+      
     </div>
   </div>
 </template>
@@ -78,16 +38,16 @@ const props = defineProps({
 });
 
 const isSubMenuActive = ref(false);
-const submenuItems = ref([]);
-const additionalItems = ref([]);
-const discriptionItems = ref([]);
-const activeIndex = ref(-1); // Добавляем отслеживание активного индекса
-let hideTimeout = null;
-const hoverEnabled = ref(true);
+// const submenuItems = ref([]);
+// const additionalItems = ref([]);
+// const discriptionItems = ref([]);
+// const activeIndex = ref(-1); // Добавляем отслеживание активного индекса
+// let hideTimeout = null;
+// const hoverEnabled = ref(true);
 
-const cancelHide = () => {
-  clearTimeout(hideTimeout);
-};
+// const cancelHide = () => {
+//   clearTimeout(hideTimeout);
+// };
 
 
 // const showSubMenu = (index) => {
@@ -107,39 +67,39 @@ const cancelHide = () => {
 
 
 
-const hideSubMenu = () => {
-  hoverEnabled.value = true;
-  hideTimeout = setTimeout(() => {
-    activeIndex.value = -1;
-    submenuItems.value = [];
-    additionalItems.value = [];
-    discriptionItems.value = [];
-    isSubMenuActive.value = false;
-    emit('update:blurActive', false);
-  }, 200); // задержка на скрытие
-};
+// const hideSubMenu = () => {
+//   hoverEnabled.value = true;
+//   hideTimeout = setTimeout(() => {
+//     activeIndex.value = -1;
+//     submenuItems.value = [];
+//     additionalItems.value = [];
+//     discriptionItems.value = [];
+//     isSubMenuActive.value = false;
+//     emit('update:blurActive', false);
+//   }, 200); // задержка на скрытие
+// };
 
-const handleClick = () => {
-  hoverEnabled.value = false;
-  hideSubMenu();
-};
+// const handleClick = () => {
+//   hoverEnabled.value = false;
+//   hideSubMenu();
+// };
 
 const emit = defineEmits();
 
-const submenuStyle = computed(() => ({
-  height: isSubMenuActive.value ? `${submenuItems.value.length * 50}px` : '0',
-  opacity: isSubMenuActive.value ? '1' : '0',
-  paddingTop: isSubMenuActive.value ? '25px' : '0',
-  paddingBottom: isSubMenuActive.value ? '35px' : '0',
-  overflow: 'hidden',
-  transition: 'height 0.3s ease, opacity 0.3s ease',
-}));
+// const submenuStyle = computed(() => ({
+//   height: isSubMenuActive.value ? `${submenuItems.value.length * 50}px` : '0',
+//   opacity: isSubMenuActive.value ? '1' : '0',
+//   paddingTop: isSubMenuActive.value ? '25px' : '0',
+//   paddingBottom: isSubMenuActive.value ? '35px' : '0',
+//   overflow: 'hidden',
+//   transition: 'height 0.3s ease, opacity 0.3s ease',
+// }));
 
-const getItemStyle = (index) => ({
-  opacity: isSubMenuActive.value ? '1' : '0',
-  transform: isSubMenuActive.value ? 'translateY(0)' : 'translateY(-10px)',
-  transition: `transform 0.3s ease ${index * 0.1}s, opacity 0.3s ease ${index * 0.1}s`,
-});
+// const getItemStyle = (index) => ({
+//   opacity: isSubMenuActive.value ? '1' : '0',
+//   transform: isSubMenuActive.value ? 'translateY(0)' : 'translateY(-10px)',
+//   transition: `transform 0.3s ease ${index * 0.1}s, opacity 0.3s ease ${index * 0.1}s`,
+// });
 </script>
 
 <style>
@@ -209,59 +169,59 @@ const getItemStyle = (index) => ({
 .header__logo:hover {
   transform: scale(1.1);
 }
-
+/* 
 .header__submenu {
   display: flex;
-  opacity: 0; /* Начальная непрозрачность */
-  height: 0;  /* Начальная высота */
-  overflow: hidden;  /* Скрытие содержимого при закрытии */
-  transition: height 0.3s ease, opacity 0.3s ease; /* Плавный переход */
+  opacity: 0; 
+  height: 0;  
+  overflow: hidden;  
+  transition: height 0.3s ease, opacity 0.3s ease; 
   padding: 0 50px;
 
-}
+} */
 
-.header__submenu-column {
+/* .header__submenu-column {
   margin-right: 65px;
-}
+} */
 
-.header__submenu-title {
+/* .header__submenu-title {
   height: 15px;
   font-size: 15px;
   margin-bottom: 20px;
-}
+} */
 
-.header__submenu-subtitle {
+/* .header__submenu-subtitle {
   transition: transform 0.3s ease, opacity 0.3s ease;
   height: 20px;
   font-size: 28px;
   font-weight: 600;
   margin-bottom: 25px;
   cursor: pointer;
-}
+} */
 
-.header__submenu-subtitle:hover {
+/* .header__submenu-subtitle:hover {
   transform: scale(1.1);
-}
+} */
 
-.header__submenu-list {
+/* .header__submenu-list {
   padding-top: 25px;
   padding-left: 0;
   margin: 0;
   list-style: none;
-}
+} */
 
-.header__submenu-item {
+/* .header__submenu-item {
   padding: 0;
   margin: 10px;
   transition: transform 0.3s ease, opacity 0.3s ease;
   height: 15px;
   font-size: 15px;
   cursor: pointer;
-}
+} */
 
-.header__submenu-item:hover {
+/* .header__submenu-item:hover {
   transform: scale(1.1);
-}
+} */
 
 @media (max-width: 900px) { /* Настроить под ваш брейкпоинт */
   .header__item {
--- components/header/NavMenu.vue
+++ components/header/NavMenu.vue
@@ -7,11 +7,11 @@
     <Header :logo="logo" :menuItems="menuItems" @update:blurActive="handleBlurChange" class="web-menu" />
 
     <!-- Размытие фона при активном состоянии -->
-    <div 
-      v-show="isBlurActive" 
+    <!-- <div 
+      v-if="isBlurActive" 
       class="blur-overlay"
       :class="{ 'fade-in': isBlurActive, 'fade-out': !isBlurActive }"
-    ></div>
+    ></div> -->
 
 
   </div>
@@ -30,61 +30,29 @@ const menuItems = [
   {
     label: 'Экскурсии',
     route: '/excursions',
-    submenu: [
-      { label: 'Кольцо Осетии', route: '/routs#ring-osetii' },
-      { label: 'Кариу Хох', route: '/routs#kariy-xox' },
-      { label: 'Верхний Мизур и Мамисон', route: '/routs#mizur-mamison' },
-      { label: 'Кармадон и Даргавс', route: '/routs#karmadon-dargavs' },
-      { label: 'Горная Дигория', route: '/routs#digoria' },
-      { label: 'Уаллаг Ком', route: '/routs#yallag-kom' },
-    ],
-    additional: [
-      { label: 'Обед', route: '/services#trump' },
-      { label: 'Встретить рассвет', route: '/services#sunrise' },
-      { label: 'Прогулка на лошадях', route: '/services#horses' },
-      { label: 'Йога', route: '/services#yoga' },
-      { label: 'Кейтринг', route: '/services#catering' },
-      { label: 'Параплан', route: '/services#paragliding' },
-      { label: 'Сплав по реке', route: '/services#rafting' },
-      { label: 'Фотограф', route: '/services#photographer' },
-      { label: 'Видеограф', route: '/services#videographer' },
-      { label: 'Аэросъемка', route: '/services#aerial' },
-    ],    
-    discription: [
-      { label: 'Как проходит экскурсия', route: '/info#how-it-works' },
-      { label: 'Групповые туры', route: '/info#group-tours' },
-      { label: 'Праздники в горах', route: '/info#holidays' },
-    ],  
   },
   { 
     label: 'Проживание', route: '/living', 
-    submenu: [
-      { label: 'В горах', route: '/living#mountains' },
-      { label: 'Во Владикавказе', route: '/living#vladikavkaz' }
-    ],  
+    
   },
   { label: 'Полный тур', 
     route: '/tours', 
-    submenu: [
-      { label: 'Фотографии', route: '/galery#photos' }
-    ], 
+    
   },
   { label: 'Контакты', 
     route: '/about', 
-    submenu: [
-        { label: 'Соц сети', route: '/about#social' },
-    ], 
+    
   },
 
 ];
 
   // Создаем переменную для состояния blur
-  const isBlurActive = ref(false);
+  // const isBlurActive = ref(false);
 
 // Функция для обновления состояния blur из дочернего компонента
-const handleBlurChange = (newValue) => {
-  isBlurActive.value = newValue;
-};
+// const handleBlurChange = (newValue) => {
+//   isBlurActive.value = newValue;
+// };
 
 
 // Define methods here if needed
@@ -102,33 +70,20 @@ const handleBlurChange = (newValue) => {
   }
 
 
-.blur-overlay {
-  z-index: 10; /* Размытие будет под подменю */
+/* .blur-overlay {
+  z-index: 10; 
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(255, 255, 255, 0.4); /* Полупрозрачный фон */
-  backdrop-filter: blur(0px); /* Размытие фона */
-  opacity: 0; /* Скрыт при изначальном состоянии */
-  transition: backdrop-filter 2s ease 1s, opacity 2s ease 1s; /* увеличили время анимации и добавили задержку */
+  background-color: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(0px); 
+  opacity: 0;
+  transition: backdrop-filter 2s ease 1s, opacity 2s ease 1s;
 
-}
+} */
 
-.blur-overlay.fade-in {
-  backdrop-filter: blur(10px); /* Добавляем размытие при активном состоянии */
-  opacity: 1; /* Плавно появляется */
-  transition: backdrop-filter 3s ease, opacity 3s ease; /* Плавная анимация исчезновения */
-
-}
-
-.blur-overlay.fade-out {
-  backdrop-filter: blur(10px); /* Добавляем размытие при активном состоянии */
-  opacity: 0; /* Плавно появляется */
-  transition: backdrop-filter 3s ease, opacity 3s ease; /* Плавная анимация исчезновения */
-
-}
 
 
 
