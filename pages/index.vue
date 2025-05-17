92975b6924b3d4bad16fbad820fcb3f7035d354d
Georgii <maratakoev@gmail.com>
2025-05-17 13:20:36 +0300

    постеры мобилки

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
       :marginTop="-70"
       :modalData="modalData[0]"
       videoSrc="/videos/hero-background.mp4"
-      videoPoster="/images/complecs-poster.webp"
+      videoPoster="/images/insta.webp"
       :lazyLoad="false"
       :ButtonSecText="ButtonSecText"
 
